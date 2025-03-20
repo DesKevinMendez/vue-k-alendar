4a74d97ae1260c57d8c0dd1061bead4e29960300
DesKevinMendez <32910223+DesKevinMendez@users.noreply.github.com>
2025-03-20 17:52:48 +0000

    Build ✍🏻: (5d4a48c) Merge pull request #6 from DesKevinMendez/fix-icon
    Fix icon

--- dist/vue-k-alendar.js
+++ dist/vue-k-alendar.js
@@ -5978,7 +5978,31 @@ const ma = /* @__PURE__ */ $(la, [["render", ha], ["__scopeId", "data-v-58b8063e
 function ka(n, e) {
   return k(), E("button", wa, va);
 }
-const Ta = /* @__PURE__ */ $(ya, [["render", ka], ["__scopeId", "data-v-bf161d91"]]), Oa = (n) => (Qe("data-v-44ee0cc7"), n = n(), Xe(), n), Sa = { class: "autor" }, Da = /* @__PURE__ */ Oa(() => /* @__PURE__ */ S("svg", {
+const Ta = /* @__PURE__ */ $(ya, [["render", ka], ["__scopeId", "data-v-bf161d91"]]), Oa = (n) => (Qe("data-v-9c08a59e"), n = n(), Xe(), n), Sa = { class: "autor" }, Da = /* @__PURE__ */ Oa(() => /* @__PURE__ */ S("svg", {
+  "aria-hidden": "true",
+  focusable: "false",
+  "data-prefix": "far",
+  "data-icon": "user",
+  xmlns: "http://www.w3.org/2000/svg",
+  viewBox: "0 0 448 512"
+}, [
+  /* @__PURE__ */ S("path", {
+    class: "",
+    fill: "currentColor",
+    d: "M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"
+  })
+], -1)), Ma = /* @__PURE__ */ W({
+  __name: "EDAutor",
+  props: {
+    autor: {}
+  },
+  setup(n) {
+    return (e, t) => (k(), E("div", Sa, [
+      Da,
+      S("p", null, G(e.autor), 1)
+    ]));
+  }
+}), Ea = /* @__PURE__ */ $(Ma, [["__scopeId", "data-v-9c08a59e"]]), Ia = (n) => (Qe("data-v-7de34190"), n = n(), Xe(), n), ba = { class: "content" }, Na = /* @__PURE__ */ Ia(() => /* @__PURE__ */ S("svg", {
   class: "h-5 w-5 text-gray-600 dark:text-gray-300",
   "aria-hidden": "true",
   focusable: "false",
@@ -5993,30 +6017,6 @@ const Ta = /* @__PURE__ */ $(ya, [["render", ka], ["__scopeId", "data-v-bf161d91
     fill: "currentColor",
     d: "M123.6 391.3c12.9-9.4 29.6-11.8 44.6-6.4c26.5 9.6 56.2 15.1 87.8 15.1c124.7 0 208-80.5 208-160s-83.3-160-208-160S48 160.5 48 240c0 32 12.4 62.8 35.7 89.2c8.6 9.7 12.8 22.5 11.8 35.5c-1.4 18.1-5.7 34.7-11.3 49.4c17-7.9 31.1-16.7 39.4-22.7zM21.2 431.9c1.8-2.7 3.5-5.4 5.1-8.1c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208s-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6c-15.1 6.6-32.3 12.6-50.1 16.1c-.8 .2-1.6 .3-2.4 .5c-4.4 .8-8.7 1.5-13.2 1.9c-.2 0-.5 .1-.7 .1c-5.1 .5-10.2 .8-15.3 .8c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4c4.1-4.2 7.8-8.7 11.3-13.5c1.7-2.3 3.3-4.6 4.8-6.9c.1-.2 .2-.3 .3-.5z"
   })
-], -1)), Ma = /* @__PURE__ */ W({
-  __name: "EDAutor",
-  props: {
-    autor: {}
-  },
-  setup(n) {
-    return (e, t) => (k(), E("div", Sa, [
-      Da,
-      S("p", null, G(e.autor), 1)
-    ]));
-  }
-}), Ea = /* @__PURE__ */ $(Ma, [["__scopeId", "data-v-44ee0cc7"]]), Ia = (n) => (Qe("data-v-3ba7380a"), n = n(), Xe(), n), ba = { class: "content" }, Na = /* @__PURE__ */ Ia(() => /* @__PURE__ */ S("svg", {
-  "aria-hidden": "true",
-  focusable: "false",
-  "data-prefix": "far",
-  "data-icon": "user",
-  xmlns: "http://www.w3.org/2000/svg",
-  viewBox: "0 0 448 512"
-}, [
-  /* @__PURE__ */ S("path", {
-    class: "",
-    fill: "currentColor",
-    d: "M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"
-  })
 ], -1)), _a = /* @__PURE__ */ W({
   __name: "EDContent",
   props: {
@@ -6028,7 +6028,7 @@ const Ta = /* @__PURE__ */ $(ya, [["render", ka], ["__scopeId", "data-v-bf161d91
       S("p", null, G(e.content), 1)
     ]));
   }
-}), xa = /* @__PURE__ */ $(_a, [["__scopeId", "data-v-3ba7380a"]]), Va = { class: "title" }, Ca = /* @__PURE__ */ W({
+}), xa = /* @__PURE__ */ $(_a, [["__scopeId", "data-v-7de34190"]]), Va = { class: "title" }, Ca = /* @__PURE__ */ W({
   __name: "EDTitle",
   props: {
     event: {}
